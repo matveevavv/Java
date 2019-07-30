5874b06825259528c5f75b4963f79f078291d53f
matveeva_v <cornix39@list.ru>
2019-07-30 15:34:11 +0700

    start

---
+++ src/lab5LocalFiles/AManageOrder.java
@@ -0,0 +1,4 @@
+package lab5LocalFiles;
+
+public abstract class AManageOrder {
+}
---
+++ src/lab5LocalFiles/IOrder.java
@@ -0,0 +1,9 @@
+package lab5LocalFiles;
+
+public interface IOrder {
+    void readById();
+    void saveDyId();
+    void readAll();
+    void saveAll();
+
+}
